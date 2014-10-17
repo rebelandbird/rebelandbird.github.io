3b12c1a50b5e8cb2889f5d549861418c1aef11b3
Stefan Pettersson <stefpet@gmail.com>
2014-10-17 11:23:51 +0200

    Allow direct links to positions

--- js/rebel.js
+++ js/rebel.js
@@ -16,5 +16,21 @@
     document.getElementById("closepositions").addEventListener("click", function() {
         Velocity(positions, "fadeOut", { duration: 200 });
     });
-})();
 
+    // open if target set to position, show that section
+    if (document.location.hash != "") {
+        Velocity(
+            positions,
+            {
+                opacity: 1
+            },
+            {
+                display: "block",
+                complete: function() {
+                    var element = document.getElementById(document.location.hash.slice(1)) || positions;
+                    Velocity(element, "scroll", { duration: 300 });
+                }
+            }
+        );
+    }
+})();
